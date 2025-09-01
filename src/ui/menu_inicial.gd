54e6a3b6ea50cb52568832508997d2f15ec919f9
Edmar Filho <ebgfilho7@gmail.com>
2025-08-31 21:53:32 -0300

    feat: combine menu logic with new scene

--- src/ui/menu_inicial.gd
+++ src/ui/menu_inicial.gd
@@ -1,21 +1,15 @@
 extends Control
 
-@export var path_to_scene_iniciar : String
-@export var top_button : Button
-var _input_enabled : bool
+@export var path_to_scene_iniciar: String
+@export var top_button: BaseButton
+var _input_enabled: bool
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_input_enabled = false;
 	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_iniciar_pressed() -> void:
-	#Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	if _input_enabled:
 		get_tree().change_scene_to_file.call_deferred(path_to_scene_iniciar)
 
