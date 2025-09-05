9a04654dd5987f1a7c1745f217212024188d66da
Edmar Filho <ebgfilho7@gmail.com>
2025-09-05 05:15:01 -0300

    feat: add demo end screen

--- entities/level/custcene_start.gd
+++ entities/level/custcene_start.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
-@export var default_timeline: String				# 
+signal cutscene_finished
+
+@export var default_timeline: String #
 
 func _get_timeline() -> String:
 	return default_timeline
@@ -9,7 +11,6 @@ func _get_timeline() -> String:
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
 		Dialogic.start(_get_timeline())
+		await Dialogic.timeline_ended
+		cutscene_finished.emit()
 		queue_free()
-		
-	
-	
